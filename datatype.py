db11b031bc4feb10c124d6c5aa3f92213535a7c4
Joel Kong <joelkong2001@gmail.com>
2023-05-07 16:08:12 +0800

    feat: learnt about string methods

--- datatype.py
+++ datatype.py
@@ -13,3 +13,11 @@ print(mystring[:3])
 print(mystring[3:6])
 print(mystring[::2])
 print(mystring[::-1]) # reverse string 
+
+name = "sam hello"
+last_letters = name[1:]
+"P" + last_letters 
+
+name.upper()
+name.split() # ['sam', 'hello'] split by white space
+name.split('i') # remove the 'i' and then split until it hits the next 'i'
