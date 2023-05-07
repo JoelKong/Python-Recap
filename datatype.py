d0b4c6d12480f48d37f007c947d40da9d8bb85f6
Joel Kong <joelkong2001@gmail.com>
2023-05-07 15:57:52 +0800

    feat: learnt about slicing

--- datatype.py
+++ datatype.py
@@ -3,3 +3,13 @@ print("Hello world")
 a = 30
 
 print(type(a))
+a = "hello"
+print(len(a))
+a[-3]
+
+mystring = 'abcdefghijk'
+print(mystring[2:])
+print(mystring[:3])
+print(mystring[3:6])
+print(mystring[::2])
+print(mystring[::-1]) # reverse string 
