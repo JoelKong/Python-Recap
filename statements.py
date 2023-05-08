2551000028d01f3254784405d6d9f19904aa8ff5
Joel Kong <joelkong2001@gmail.com>
2023-05-08 12:35:09 +0800

    feat: learnt about while loops, lib, enumerate and zip

--- statements.py
+++ statements.py
@@ -17,3 +17,28 @@ for (a,b) in mylist:
 d = {'k1': 1, 'k2' : 2, 'k3': 3}
 for key,value in d.items():
     print(value)
+
+for num in range(0,11,2):
+    print(num)
+
+list(range(0,11,2))
+
+for item in enumerate('abcde'):
+    print(item)
+
+list1 = [1,2,3]
+list2 = [4,5,6]
+
+for item in zip(list1,list2):
+    print(item)
+
+min(list1)
+max(list2)
+
+#libraries
+from random import shuffle
+from random import randint
+shuffle(mylist)
+int(input('enter'))
+
+mylist = [letter for letter in list1]
