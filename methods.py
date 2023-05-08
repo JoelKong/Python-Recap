3a173a3ea6288d557bd0d81d37fd6d742587af3d
Joel Kong <joelkong2001@gmail.com>
2023-05-08 14:14:37 +0800

    feat: learnt about map and filter

--- methods.py
+++ methods.py
@@ -10,4 +10,13 @@ def myfunc(**kwargs):
     if 'fruit' in kwargs:
         print('fruit {}'.format(kwargs['fruit']))   # kwargs is converted to dictionary (key word arg)
 
-myfunc(fruit='apple', veggie = 'lettuce') # {'fruit: 'apple', 'veggie': 'lettuce}
+myfunc(fruit='apple', veggie = 'lettuce') # {'fruit: 'apple', 'veggie': 'lettuce}\
+
+
+def square(num):
+    return num**2
+
+my_nums = [1,2,3,4,5]
+for item in map(square, my_nums):
+    print(item)
+
